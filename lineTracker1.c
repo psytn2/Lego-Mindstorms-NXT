7fca9bd66fd48325f4892e012db9bc4e22b8e54f
Tai Nguyen Bui <tnb1990@gmail.com>
2013-10-08 14:34:17 +0100

    First G53ARS lab session
    The code is implemented for a Lego Mindstorm NXT robot to follow a black
    line

---
+++ lineTracker1.c
@@ -0,0 +1,21 @@
+#pragma config(Sensor, S1,	lightSensor,	sensorLightActive)
+
+task main(){
+
+	while(1==1){ // infinite loop
+		while(SensorValue(lightSensor) < 58){ // while sensor is not on the black line turn right
+			motor[motorA] = 50;
+			motor[motorC] = 0;
+		}
+		//* while robot is on the black line, go straight
+		while(SensorValue(lightSensor) >= 58 && SensorValue(lightSensor) <= 78){
+			motor[motorA] = 70;
+			motor[motorC] = 70;
+		}
+  
+		while(SensorValue(lightSensor) > 78){ // while sensor is not on the black line turn left
+			motor[motorA] = 0;
+			motor[motorC] = 50;
+		}
+	}
+}
